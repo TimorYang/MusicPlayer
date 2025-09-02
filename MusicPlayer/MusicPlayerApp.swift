98ef36dd76fc38d42be7b6fa195304d338f7063c
TimorYang <iyangzhidong@gmail.com>
2025-09-02 17:26:15 +0800

    feat: initialise the project

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by TeemoYang on 2025/9/2.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
